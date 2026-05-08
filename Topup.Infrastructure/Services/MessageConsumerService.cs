1db93885829dcd7bc2f31a37cfcc3debdff004c8
ZohrehMalekiAsl <malekiaslzohreh@Gmail.com>
2026-05-08 16:59:49 +0330

    handle duplicate error in Rabbit mq

--- Topup.Infrastructure/Services/MessageConsumerService.cs
+++ Topup.Infrastructure/Services/MessageConsumerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
@@ -11,7 +12,7 @@ using Topup.Domain.Repositories;
 
 namespace Topup.Infrastructure.Services
 {
-    public class MessageConsumerService: IMessageConsumerService
+    public class MessageConsumerService : IMessageConsumerService
     {
         private readonly IChargeRequestRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
@@ -19,7 +20,7 @@ namespace Topup.Infrastructure.Services
         private readonly IAppSetting _appSetting;
 
         public MessageConsumerService(IChargeRequestRepository repository, IUnitOfWork unitOfWork,
-            ILogService<MessageConsumerService> logger , IAppSetting appSetting)
+            ILogService<MessageConsumerService> logger, IAppSetting appSetting)
         {
             _repository = repository;
             _unitOfWork = unitOfWork;
@@ -43,23 +44,41 @@ namespace Topup.Infrastructure.Services
             {
                 try
                 {
-                    var body = Encoding.UTF8.GetString(ea.Body.ToArray());
-                    //var result = new ChargeRequest();
-                    //result.Amount = "20000";
-                    //result.PhoneNumber = "09126933297";
-                    //result.TerminalId = "05";
-                    //result.SystemTrace = Guid.NewGuid().ToString();
+                var body = Encoding.UTF8.GetString(ea.Body.ToArray());
+                //var result = new ChargeRequest();
+                //result.Amount = "20000";
+                //result.PhoneNumber = "09126933297";
+                //result.TerminalId = "05";
+                //result.SystemTrace = "2";
 
-                    var result = JsonSerializer.Deserialize<ChargeRequest>(body);
-                    if (result is null)
-                        throw new InvalidOperationException("Invalid message payload.");
+                var result = JsonSerializer.Deserialize<ChargeRequest>(body);
+                if (result is null)
+                {
+                    _logger.LogData(LogLevel.Error, body + " Invalid Message");
+                        await channel.BasicNackAsync(ea.DeliveryTag, false, requeue: false);
+                        return;
+                }
 
-                    result.Status = Status.Pending.ToString();
+                result.Status = Status.Pending.ToString();
+                    try
+                    {
+                        _repository.Add(result);
+                        await _unitOfWork.SaveAysnc();
+                        await channel.BasicAckAsync(ea.DeliveryTag, multiple: false);
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        if (IsDuplicateKey(ex))
+                        {
+                            _logger.LogData(LogLevel.Error, "Duplicate detected." + ex.ToString());
 
-                    _repository.Add(result);
-                    await _unitOfWork.SaveAysnc();
-
-                    await channel.BasicAckAsync(ea.DeliveryTag, multiple: false);
+                            await channel.BasicAckAsync(ea.DeliveryTag, false);
+                            return;
+                        }
+                        _logger.LogData(LogLevel.Error, ex.Message + " Doblicate Message");
+                        await channel.BasicNackAsync(ea.DeliveryTag, false, requeue: true);
+                        return;
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -74,6 +93,15 @@ namespace Topup.Infrastructure.Services
                 consumer: consumer,
                 cancellationToken: ct
             );
+        }
+        private bool IsDuplicateKey(DbUpdateException ex)
+        {
+            var inner = ex.InnerException?.ToString();
+            if (inner == null) return false;
+
+            return inner.Contains("duplicate", StringComparison.OrdinalIgnoreCase)
+                || inner.Contains("unique", StringComparison.OrdinalIgnoreCase)
+                || inner.Contains("UNIQUE constraint", StringComparison.OrdinalIgnoreCase);
         }
 
     }
